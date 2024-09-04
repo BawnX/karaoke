eaa699c104110ee54d9bb21e4e7497d2bc9fe8ef
Antonio Morales <antoniomoralesrocha@gmail.com>
2024-09-04 16:30:04 -0400

    feat: 🎸 basic button

--- src/admin/components/navbar/index.tsx
+++ src/admin/components/navbar/index.tsx
@@ -1,19 +1,20 @@
 import Icon from "@/common/components/icon";
 import styles from './navbar.module.css';
 import { Button } from "@/common/components/button";
+import HomeIcon from "@/common/icons/home";
 
 export function Navbar() {
     return (
         <nav>
             <section className={styles.navRow}>
-                <header style={{ display: 'flex', placeItems: 'center', marginLeft: "1rem" }}>
+                <header>
                     <Icon height={64} width={54} />
-                    <strong style={{ fontSize: '2.369rem', marginLeft: '0.5rem' }}>Allian</strong>
+                    <strong className={styles.logoText}>Allian</strong>
                 </header>
                 <main>
-                    <Button>1</Button>
-                    <button>2</button>
-                    <button>3</button>
+                    <Button color="background-200" hover="background-300">
+                        <div className={styles.buttonContent}><HomeIcon /><strong>Inicio</strong></div>
+                    </Button>
                 </main>
                 <footer>
                     <p>footer</p>
--- src/common/components/button/index.tsx
+++ src/common/components/button/index.tsx
@@ -1,12 +1,25 @@
-import { ReactNode } from "react"
+import { CSSProperties, ReactNode } from "react"
+import styles from './button.module.css'
 
 interface ButtonProps {
     children: ReactNode,
+    color: string,
+    hover: string,
 }
 
-export function Button({ children }: ButtonProps) {
+interface CustomCSSProperties extends CSSProperties {
+    '--color'?: string;
+    '--hover'?: string;
+}
+
+export function Button({ children, color, hover }: Readonly<ButtonProps>) {
+    const customCss: CustomCSSProperties = {
+        '--color': `var(--${color})`,
+        '--hover': `var(--${hover})`
+    }
+
     return (
-        <button style={{ borderRadius: '20px', padding: '0.5rem' }}>
+        <button className={styles.button} style={customCss}>
             {children}
         </button>
     )
